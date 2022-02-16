fd26a370acbf0add0fbbfe9d3857a2ee779fc7fa
Becca Byrne <aggiebec06@gmail.com>
2022-02-16 11:44:28 -0600

    created manifest

--- public/js/idb.js
+++ public/js/idb.js
@@ -6,7 +6,7 @@ const request = indexedDB.open('budget_tracker', 1);
 
 // this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
 request.onupgradeneeded = function (event) {
-    // save a reference to the database 
+
     const db = event.target.result;
     // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts 
     db.createObjectStore('new_transaction', { autoIncrement: true });
@@ -29,7 +29,7 @@ request.onerror = function (event) {
 };
 
 // This function will be executed if we attempt to submit a new transaction and there's no internet connection
-function saveRecord(record) {
+function saveRecord(transaction) {
     // open a new transaction with the database with read and write permissions 
     const transaction = db.transaction(['new_transaction'], 'readwrite');
 
@@ -37,7 +37,7 @@ function saveRecord(record) {
     const budgetObjectStore = transaction.objectStore('new_transaction');
 
     // add record to your store with add method
-    budgetObjectStore.add(record);
+    budgetObjectStore.add(transaction);
 }
 
 function uploadTransaction() {
